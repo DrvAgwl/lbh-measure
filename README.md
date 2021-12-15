076c6773d49a7bea71f8fbcd4ca805ac481e0491
Nikhil Kasukurthi <nikhil.k@udaan.com>
2021-12-15 14:23:39 +0530

    Update serivce name in prometheus-config.yaml

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
-# Blank Python Project
-Sample Project Template for Python Services in Udaan
+## LBH Measure Python
+This repository contains the SDK for LBH Measure. 
+It can be used for training and deployment 
 
-### Note:
-- This service architecture is mainly focussed to deploy DS/ML Model Services but can be easily modified as per other use-case needs
-- This service uses github actions for CI/CD
-_[*Recommended*]_ To leverage the standard DS/ML Slack Notifications Pipeline reach-out to [ML Engineering]( https://github.com/orgs/udaan-com/teams/ml-eng/members ) on `#ml-eng-products` channel
-  - Add `DS_SLACK_TOKEN` secret contains slack bot token to repo secrets, by default build-script will fail without it
-  - Scheduled Update will be enabled by default when this template is used, to disable it 
-    - Go to `Actions > Click on Scheduled Model Update(Workflow) > Click on three dots(...) > Choose Disable Workflow`
+## Training
 
+## Testing
 
-### Best Practices [ _Strongly Recommended_ ]
-- Each service should install packages only via Pipfile using `pipenv install <package-name>`
-- `requirements.txt` should be generated automatically from Pipfile by running the following command
-  - `pipenv lock -r | awk 'NR > 8 { print }' > requirements.txt` in the project home
+## Deployment
 
-
-### Run in Local
-- `cd resources`
-- `sh dev-server.sh`
---
+++ lbh_measure/annotation_validation.py
@@ -0,0 +1,95 @@
+import os
+from tqdm import tqdm
+import argparse
+import json
+import numpy as np
+import open3d as o3d
+from glob import glob
+
+
+class ValidateAnnotation:
+    """
+    Annotation Directory Structure
+    Base
+     |--Annotator 1
+     |   |-- pcd1.json
+     |   |-- pcd2.json
+     |--Annotator 2
+     |   |-- pcd1.json
+     |   |-- pcd1.jso
+    """
+
+    def __init__(self, annotation_base_dir, input_pcd_dir):
+        self.annotation_base_dir = annotation_base_dir
+        self.input_pcd_dir = input_pcd_dir
+
+    def parse_annotation(self, data: dict, label='cart'):
+        """
+        The annotation form LabelCloud3D.
+        We are finding the label as "cart"
+        :param data: (dict) This is the JSON file object from the tool.
+        :param label: The label to fetch annotations for. Default - cart.
+        :return: centroid, rotations, dimensions
+        """
+        centroid = {}
+        rotations = {}
+        dimensions = {}
+        for objects in data["objects"]:
+            if objects["name"] == label:
+                object_roi = data["objects"][0]
+                centroid = object_roi["centroid"]
+                dimensions = object_roi["dimensions"]
+                rotations = object_roi["rotations"]
+        return centroid, rotations, dimensions
+
+    def create_bounding_box(self, centroid: dict, rotations: dict, dimensions: dict):
+        """
+        Method to create the OrientedBounding box for open3d visualiation
+        :param centroid:
+        :param rotations:
+        :param dimensions:
+        :return:
+        """
+        center = np.array([centroid["x"], centroid["y"], centroid["z"]], dtype=np.float64)
+        rotations_np = np.radians(np.array([rotations["x"], rotations["y"], rotations["z"]], dtype=np.float64))
+        R = o3d.geometry.OrientedBoundingBox.get_rotation_matrix_from_axis_angle(rotations_np)
+
+        extent = np.array([dimensions["length"], dimensions["width"], dimensions["height"]], dtype=np.float64)
+
+        ob = o3d.geometry.OrientedBoundingBox(center, R, extent)
+        return ob
+
+    def validate_annotation(self):
+        file_names = [i.split('/')[-1].split('.')[0] for i in glob(glob(f'{self.annotation_base_dir}/*')[0]+'/*.json')]
+        for file in file_names:
+            pcd = o3d.io.read_point_cloud(
+                os.path.join(self.input_pcd_dir, f'{file}.pcd')
+            )
+            vis_boxes = [pcd]
+            for i in glob(f'{self.annotation_base_dir}/*'):
+                with open(os.path.join(i, f'{file}.json'), 'r') as f:
+                    data = json.load(f)
+                centroid, rotations, dimensions = self.parse_annotation(data)
+                box = self.create_bounding_box(centroid, rotations, dimensions)
+                vis_boxes.append(box)
+            o3d.visualization.draw_geometries(vis_boxes)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert from rosbag to pcd")
+    """
+    Annotation Directory Structure
+    Base
+     |--Annotator 1
+     |   |-- pcd1.json
+     |   |-- pcd2.json
+     |--Annotator 2
+     |   |-- pcd1.json
+     |   |-- pcd1.json
+    """
+    parser.add_argument("--annotation_dir", type=str, required=True,
+                        help="Directory with multiple user annotation files")
+    parser.add_argument("--input_pcd_dir", type=str, required=True, help="Directory with Point Cloud files")
+    args = parser.parse_args()
+    validate_annos = ValidateAnnotation(args.annotation_dir, args.input_pcd_dir)
+    validate_annos.validate_annotation()
