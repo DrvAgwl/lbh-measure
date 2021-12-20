a180d49889a19e2b7202e87f952ce05d58e50a84
Nikhil Kasukurthi <nikhil.k@udaan.com>
2021-12-20 20:48:51 +0530

    Install through pip

--- resources/prod-server.sh
+++ resources/prod-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 pip install lbh_measure-1.0-py3-none-any.whl --no-deps
-pip install -U open3d
-apt-get install libgl1
-apt install -y libgl1-mesa-glx
+pip install https://storage.googleapis.com/open3d-releases-master/python-wheels/open3d-0.14.1-cp37-cp37m-manylinux_2_27_x86_64.whl
 uvicorn main:app --host 0.0.0.0 --port 8000 --log-level info
