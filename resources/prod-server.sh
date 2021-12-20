2c87934f13dcd3a7f491e719cfba75070927dc65
Nikhil Kasukurthi <nikhil.k@udaan.com>
2021-12-20 20:30:06 +0530

    Add opengl libs

--- resources/prod-server.sh
+++ resources/prod-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pip install lbh_measure-1.0-py3-none-any.whl --no-deps
 pip install -U open3d
+apt-get install libgl1
+apt install -y libgl1-mesa-glx
 uvicorn main:app --host 0.0.0.0 --port 8000 --log-level info
