a315d3869a8c059b3f9581db02861880ae79421c
Nikhil Kasukurthi <nikhil.k@udaan.com>
2021-12-20 16:22:21 +0530

    Add local install

--- resources/prod-server.sh
+++ resources/prod-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install git+https://github.com/udaan-com/lbh-measure-python-service.git@6243bcdb31df21132a1051fb4d9b8939a47f313a#egg=lbh-measure
+pip install ..
 uvicorn main:app --host 0.0.0.0 --port 8000 --log-level info
