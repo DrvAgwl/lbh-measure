78ae5cc3c7b56fcb34d663aad3ba8bd51c432b7a
Nikhil Kasukurthi <nikhil.k@udaan.com>
2021-12-21 08:57:18 +0530

    Update constants path

--- server/main.py
+++ server/main.py
@@ -2,14 +2,13 @@ import omegaconf
 import torch
 import uvicorn
 from udaan_common.logging import logger
-from udaan_common.resources.cosmos.cosmos_client_builder import CosmosClientBuilder
 from udaan_common.server import create_fast_api_server
 
 from lbh_measure.model_inference import load_model
 from lbh_measure.model_inference import main, download_file
 from lbh_measure.utils.convert_rosbag_to_pcd import ConvertToPCD
 from lbh_measure.utils.request_types import PredictVolumeFields
-from lbh_measure.utils import constants
+from server import constants
 
 app = create_fast_api_server()
 
