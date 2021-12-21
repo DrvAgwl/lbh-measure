73289d78a3c7a4a104f06e016afaca0e273ca43a
Nikhil Kasukurthi <nikhil.k@udaan.com>
2021-12-21 08:45:20 +0530

    Update model path

--- lbh_measure/utils/constants.py
+++ lbh_measure/utils/constants.py
@@ -1,7 +1,7 @@
 #  Add any constants/environment variables here
 
 # Get the Model from mounted model share store
-ML_MODEL_PATH = "/models/hardware/dev/v0/epoch=15-step=79.ckpt"
+ML_MODEL_PATH = "/models/hardware/v0/epoch=15-step=79.ckpt"
 COSMOS_DATABASE_NAME = 'measure'
 COSMOS_CONTAINER_NAME = 'audit'
 BLOB_CONTAINER_NAME = 'tcprofilerimages'
